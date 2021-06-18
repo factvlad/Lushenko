f2f7ee9cbac3c79e96a29736745d3c50db690cc8
FACT Vlad <email@example.com>
2021-06-18 18:00:37 +0300

    no message

---
+++ JavaScript 2.0/unit_29/Goods2.js
@@ -0,0 +1,11 @@
+class Goods2 extends Goods {
+  constructor(name, amount, image, count, sale) {
+    super(name, amount, image, count);
+    this.sale = sale;
+  }
+
+  draw(element) {
+    super.draw(element);
+    // document.querySelector(element).innerHTML += ` ${this.sale}`;
+  }
+}
---
+++ JavaScript 2.0/unit_30/Valid.js
@@ -0,0 +1,13 @@
+class Valid {
+    constructor(email, password) {
+      this.email = email;
+      this.password = password;
+      this.isValid = false;
+    }
+    validate() {
+      if (this.password.length >= 6) {
+        this.isValid = true;
+      }
+    }
+  }
+
