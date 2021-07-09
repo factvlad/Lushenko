c4d90d5245102b5ff4d72d401aa8d7736acdcb64
Vlad <71657155+factvlad@users.noreply.github.com>
2021-07-09 13:41:10 +0300

    up

---
+++ ReactJS/unit_07_1/src_1/App.js
@@ -0,0 +1,53 @@
+import "./App.css";
+import goodsArr from "./goods.json";
+import React, { useState } from "react";
+import Goods from "./Goods";
+import CartComponent from "./CartComponent";
+
+function App() {
+  const [cart, setCart] = useState({});
+  const [count, setCount] = useState(0);
+
+  function addToCart(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains("add-to-cart")) return false;
+    let cartTemp = cart;
+    cartTemp[event.target.dataset.key]
+      ? cartTemp[event.target.dataset.key]++
+      : (cartTemp[event.target.dataset.key] = 1);
+    setCart(cartTemp);
+    console.log(cart);
+    let count1 = count;
+    count1++;
+    setCount(count1);
+  }
+
+  // let showCart;
+  // if (count !== 0) {
+  //   showCart = <Cart cart={cart} goods={goodsArr} />;
+  // } else {
+  //   showCart = "Empty";
+  // }
+
+  return (
+    <>
+      <div className="container">
+        <h1>Cart</h1>
+        <div className="goods-field" onClick={addToCart}>
+          {goodsArr.map((item) => (
+            <Goods
+              title={item.title}
+              cost={item.cost}
+              image={item.image}
+              articul={item.articul}
+              key={item.articul}
+            />
+          ))}
+        </div>
+        <CartComponent data={cart}/>
+      </div>
+    </>
+  );
+}
+
+export default App;
