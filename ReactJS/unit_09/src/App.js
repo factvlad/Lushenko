847ea409e9cc8b5da0ef9d09668f86c0af684b84
Vlad <71657155+factvlad@users.noreply.github.com>
2021-07-16 21:36:17 +0300

    up

--- ReactJS/unit_09/src/App.js
+++ ReactJS/unit_09/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+// import PlaceholderPostHook from "./PlaceholderPostHook";
+import CommentHook from "./CommentHook";
+// import PlaceholderPost from "./PlaceholderPost";
+import Comment from "./Comment";
+import Comment2 from "./Comment2";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <PlaceholderPost /> */}
+      {/* <Comment/> */}
+      <Comment2/>
+      {/* <PlaceholderPostHook /> */}
+      {/* <CommentHook/> */}
+
+    </>
   );
 }
 
