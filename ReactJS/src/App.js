c99036b80f9d5de6c0a560c9293538434a33f1c2
Vlad <71657155+factvlad@users.noreply.github.com>
2021-07-19 23:45:41 +0300

    up

---
+++ ReactJS/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import PlaceholderPostHook from './PlaceholderPostHook'
+import CommentHook from './CommentHook'
+import CommentListHook from './CommentListHook'
+import Comment2ListHook from './Comment2ListHook'
+import Comment2Hook from './Comment2Hook'
+
+function App() {
+  return (
+    <>
+      <PlaceholderPostHook />
+      <CommentHook />
+    </>
+  );
+}
+
+export default App;
--- ReactJS/unit_10/src/App.js
+++ ReactJS/unit_10/src/App.js
@@ -1,18 +1,13 @@
-import './App.css';
-import PlaceholderPostHook from './PlaceholderPostHook';
-import CommentHook from './CommentHook';
-import Comment2Hook from './Comment2Hook';
-// import CommentsListHook from './CommentsListHook';
-// import Comments2ListHook from './Comments2ListHook';
-
+import "./App.css";
+import In from "./In";
+import Out from "./Out";
 
 function App() {
   return (
-    <>
-      {/* <PlaceholderPostHook /> */}
-      {/* <CommentHook /> */}
-      <Comment2Hook />
-    </>
+    <div className="App">
+      <In />
+      <Out />
+    </div>
   );
 }
 
